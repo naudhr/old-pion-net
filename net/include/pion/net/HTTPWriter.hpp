da655438fba90a58f258a05d8024db7246b5e366
Michael Dickey <mike@atomiclabs.com>
2007-11-30 00:32:15 +0000

    http://pion.atomiclabs.com/trac/ticket/63 Fixed bug causing chunks to be sent even if not supported by client (HTTP/1.0)

--- net/include/pion/net/HTTPWriter.hpp
+++ net/include/pion/net/HTTPWriter.hpp
@@ -228,6 +228,9 @@ public:
 	/// returns the length of the payload content (in bytes)
 	inline size_t getContentLength(void) const { return m_content_length; }
 
+	/// sets whether or not the client supports chunked messages
+	inline void supportsChunkedMessages(bool b) { m_client_supports_chunks = b; }
+	
 	/// returns true if the client supports chunked messages
 	inline bool supportsChunkedMessages() const { return m_client_supports_chunks; }
 
